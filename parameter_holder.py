c5b2d954e6a465d1b661c35aba50ec7c795ebbe7
MolMed2 <79400636+MolMed2@users.noreply.github.com>
2021-03-26 13:14:13 +0100

    Update credits

--- parameter_holder.py
+++ parameter_holder.py
@@ -5,7 +5,7 @@ __author__ = "Philipp Lang and Raphael Kronberg Department of Molecular Medicine
 __license__ = "MIT"
 __version__ = "1.0.1"
 __status__ = "Prototype: This progam/code can not be used as diagnostic tool."
-__credits__ = "Pls cite, when using the code: Werner J., Kronberg R. et al., Deep Transfer Learning approach" \
+__credits__ = "Pls cite and refer to when using the code: Werner J., Kronberg R. et al., Deep Transfer Learning approach" \
               " for automatic recognition of drug toxicity and inhibition of SARS-CoV-2, Viruses, 2021"
 
 def create_arg_dict(reload=True,
